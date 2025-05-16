f25a95c9cc91d9165616da28dfeb8565d4db5db6
omerfarukozcan <omerfrk_0001@hotmail.com>
2025-05-16 12:40:18 +0300

    alt yapı değişiklikleri

---
+++ src/main/java/com/omerfarukozcan/RestorancalismaApplication.java
@@ -0,0 +1,12 @@
+package com.omerfarukozcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorancalismaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestorancalismaApplication.class, args);
+    }
+}
