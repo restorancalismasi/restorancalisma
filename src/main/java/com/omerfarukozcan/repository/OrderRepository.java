869f0f8fa5a8a2091fd8ddda2c45ede77f0d1923
tarikk <teozkul@gmail.com>
2025-05-17 16:53:50 +0300

    repository e gerekli guncellemeleri yaptım classlar eklendi

---
+++ src/main/java/com/omerfarukozcan/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package com.omerfarukozcan.repository;
+
+import com.omerfarukozcan.entity.OrderModel;
+import com.omerfarukozcan.entity.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderModel, Long> {
+
+    List<OrderModel> findAllByEmailOrderByUpdatedAtDesc(String email);
+
+    List<OrderModel> findAllBySessionIdOrderByUpdatedAtDesc(String sessionId);
+
+    List<OrderModel> findAllByOrderCodeAndStatusOrderByUpdatedAtDesc(String orderCode, Status status);
+
+    List<OrderModel> findAllByStatusOrderByUpdatedAtDesc(Status status);
+
+}
