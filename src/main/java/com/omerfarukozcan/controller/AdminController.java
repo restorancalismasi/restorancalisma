5d9ca7cb8fdf2745cc2e4fcf3cdda1c1f8d98b63
Sadakazam <230202139@kocaeli.edu.tr>
2025-05-28 03:43:42 +0300

    Entity Branchin ilk adımı

--- src/main/java/com/omerfarukozcan/controller/AdminController.java
+++ src/main/java/com/omerfarukozcan/controller/AdminController.java
@@ -6,8 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;cd restorancalisma
+
 
 import static com.omerfarukozcan.util.CommonUtil.*;
 
